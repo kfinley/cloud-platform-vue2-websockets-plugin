0805069696b2f951265833fa5a271da893ed78cf
kfinley <kyle@kylefinley.net>
2021-11-14 07:59:27 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# vue2-websockets
+
+## Overview
+@cloud-platform/vue2-websockets-plugin is a Vue2 plugin that provides WebSockets communication features.
---
+++ src/store/webSocketsModule.ts
@@ -0,0 +1,52 @@
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+import { WebSocketsState, WebSocketsStatus } from './state';
+import { authHelper } from '@cloud-platform/api-client/src/helpers'
+import Sockette from 'sockette';
+import { Socket } from '../models';
+
+@Module({ namespaced: true, name: 'WebSockets' })
+export class WebSocketsModule extends VuexModule implements WebSocketsState {
+  status: WebSocketsStatus = WebSocketsStatus.None;
+
+  socket!: Socket;
+
+  @Action
+  handleSocketMessage(ev: MessageEvent) {
+
+    const { subject, message } = JSON.parse(ev.data);
+    const [module, action] = subject.split('-');
+
+    const toCamelCase = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
+
+    this.context.dispatch(`${module}/${toCamelCase(action)}`, message, { root: true });
+  };
+
+  @Action
+  handleSocketClose(ev: CloseEvent) {
+    console.log('handleSocketClose', ev);
+  }
+
+  @Action
+  connect(url: string) {
+
+    const wsUrl = `ws://${url}`;
+
+    console.log(`conncting to socket: ${wsUrl}`);
+
+    this.context.commit('mutate', (state: WebSocketsState) => {
+      state.socket = new Sockette(wsUrl, {
+        protocols: authHelper.authToken(),
+        onmessage: this.handleSocketMessage,
+        // onreconnect?: (this: Sockette, ev: Event | CloseEvent) => any;
+        // onmaximum?: (this: Sockette, ev: CloseEvent) => any;
+        onclose: this.handleSocketClose,
+        //  onerror?: (this: Sockette, ev: Event) => any;
+      })
+    });
+  }
+
+  @Mutation
+  mutate(mutation: (state: WebSocketsState) => void) {
+    mutation(this);
+  }
+}
